b881e20ea803c552ae097affab0c98b7a8cc06b0
aayushsingh7 <97450185+aayushsingh7@users.noreply.github.com>
2026-03-07 23:59:14 +0530

    refactor: update home page UI

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,41 @@
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
+import { useNavigate } from "react-router-dom";
+import { useGuestAccount } from "../hooks/useGuestAccount";
+import DropDown from "../components/DropDown";
+import {ArrowLongRightIcon, ArrowPathIcon} from "@heroicons/react/24/outline"
 
 const navigation = [];
+const productCategories = [
+  "Headphones",
+  "Watch",
+  "Shoe",
+  "Bag",
+  "Sunglasses",
+  "Mobile Phone",
+  "Laptop",
+  "Jacket",
+  "Shirt",
+  "Cosmetics"
+];
 
 const Home = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const guestId = useGuestAccount();
+  const navigate = useNavigate();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const createNewJob = async (e: any) => {};
 
   return (
-    <div className="bg-gray-900">
+    <div className="polka-bg">
+      {/* <DropDown options={productCategories} /> */}
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
           aria-label="Global"
           className="flex items-center justify-between p-6 lg:px-8"
         >
-          <div className="flex lg:flex-1">
-           
-          </div>
+          <div className="flex lg:flex-1"></div>
           <div className="flex lg:hidden">
             <button
               type="button"
@@ -96,24 +116,36 @@ const Home = () => {
       <div className="font-inter relative isolate px-6 pt-40 lg:px-8 h-[100dvh] overflow-hidden">
         <div className="mx-auto w-full flex items-center justify-center flex-col">
           <h1 className="relative leading-tight text-center text-4xl max-w-5xl  font-semibold tracking-tight text-balance text-white sm:text-7xl">
-           From Video to <span className="text-indigo-600">Amazon Best-Seller</span> in <span className="relative">
-             <span className="absolute right-0 w-full h-[4px] bottom-[-3px] bg-indigo-600 block"></span>
-             <span className="absolute left-0 w-[80%] h-[4px] bottom-[-15px] bg-indigo-600 block"></span>
-             Minutes</span>.
+            From Video to{" "}
+            <span className="text-indigo-600">Amazon Best-Seller</span> in{" "}
+            <span className="relative">
+              <span className="absolute right-0 w-full h-[4px] bottom-[-3px] bg-indigo-600 block"></span>
+              <span className="absolute left-0 w-[80%] h-[4px] bottom-[-15px] bg-indigo-600 block"></span>
+              Minutes
+            </span>
+            .
           </h1>
-          <p className="mt-10 text-2xl max-w-2xl text-gray-400 text-center">Stop wasting days on copywriting - let AI extract the gold from your post and dominate the A9 algorithm.</p>
-          <form className="mt-10 max-w-2xl flex items-center border gap-2 bg-gray-800 border-gray-500/30 h-17  w-full rounded-full overflow-hidden">
+          <p className="mt-10 text-2xl max-w-2xl text-gray-400 text-center">
+            Stop wasting days on copywriting - let AI extract the gold from your
+            post and dominate the A9 algorithm.
+          </p>
+          <form
+            onSubmit={createNewJob}
+            className="mt-10 max-w-3xl flex items-center border gap-2 bg-gray-800 border-gray-500/30 h-17  w-full rounded-full"
+          >
+            <DropDown options={productCategories} defaultTxt="Category" />
             <input
-              type="text"
-              placeholder="Enter Youtube Shorts or Instagram Reel Link"
-              className="w-full h-full pl-6 outline-none text-xl placeholder-gray-400 text-white"
+              type="url"
+              placeholder="Enter Instagram Post or Reel Link"
+              className="w-full h-full pl-2 outline-none text-xl placeholder-gray-400 text-white"
               required
             />
             <button
+            disabled={loading}
               type="submit"
-              className="bg-indigo-600 active:scale-95 transition w-56 h-14  rounded-full text-xl font-bold text-white mr-1 flex items-center justify-center"
+              className="bg-indigo-600 active:scale-95 transition w-30 h-14  rounded-full text-xl font-bold text-white mr-1 flex items-center justify-center"
             >
-              Generate
+              {loading ? <ArrowPathIcon className="w-8 h-8 font-bold" style={{animation: "spin 1.4s linear infinite" }} /> : <ArrowLongRightIcon className="w-10 h-10"/>}
             </button>
           </form>
         </div>
