756d20763feff19b8de317a1a0298057cd512efa
aayushsingh7 <97450185+aayushsingh7@users.noreply.github.com>
2026-03-08 01:45:38 +0530

    feat: add logic for ingestion worker

--- backend/src/worker/ingestion.worker.js
+++ backend/src/worker/ingestion.worker.js
@@ -2,14 +2,85 @@ import dotenv from "dotenv";
 dotenv.config();
 import {Worker} from "bullmq";
 import redis from "../shared/config/redis.config.js";
+import AIService from "./services/ai.service.js";
+import AWSService from "../shared/services/aws.service.js";
+import ScraperService from "./services/scraper.service.js";
+import {Emitter} from "@socket.io/redis-emitter";
+import ListingService from "../shared/services/listing.service.js";
+import {processingQueue} from "../shared/config/queue.config.js";
+import connectMongo from "../shared/config/mongo.config.js";
+
+connectMongo();
+const emitter = new Emitter(redis);
+const awsService = new AWSService();
+const listingService = new ListingService();
+const scraperSerivce = new ScraperService(awsService);
+const aiService = new AIService(awsService);
 
 const worker = new Worker(
     "ingestion-queue",
     async (job) => {
         if (job.name === "ingest-job") {
             console.log("[STARTED INGESTION JOB]");
-            const {url} = job.data;
-            console.log({url})
+            const {url, productType, userId, primarySourceUrl} = job.data;
+            let jobId = job.id;
+            try {
+                emitter.to(userId).emit("job-status", {jobId, status: "INGESTING_AND_VERIFYING", errorMessage: ""});
+                await listingService.updateProcessingStatus({
+                    currentStatus: "INGESTING_AND_VERIFYING",
+                    jobId,
+                });
+
+                postData = await scraperSerivce.fetchInstagramReel(url);
+
+                const result = await aiService.analyzeVideoContent({
+                    s3VideoKey: postData.s3Key,
+                    title: postData.title,
+                    description: postData.description,
+                    productType,
+                });
+
+                let jobStatus = "QUEUED",
+                    errorMessage = "";
+                if (!result.isRejected) {
+                    jobStatus = "REJECTED";
+                    errorMessage = result.response.join("\n");
+                    await listingService.updateProcessingStatus({
+                        currentStatus: jobStatus,
+                        jobId,
+                        errorMessage: result.reasons.join("\n"),
+                    });
+                } else {
+                    await processingQueue.add(
+                        "process-video",
+                        {
+                            s3Key: postData.s3Key,
+                            userId,
+                            primarySourceUrl,
+                            videoAnalysisResult: result,
+                            postMetadata: postData,
+                        },
+                        {jobId}
+                    );
+                    await listingService.updateProcessingStatus({
+                        currentStatus: jobStatus,
+                        jobId,
+                    });
+                }
+                emitter.to(userId).emit("job-status", {jobId, status: jobStatus, errorMessage});
+            } catch (err) {
+                await listingService.updateProcessingStatus({
+                    currentStatus: "FAILED",
+                    jobId: job.id,
+                    errorMessage: "Something Went Wrong",
+                });
+
+                emitter
+                .to(job.data.userId)
+                .emit("job-status", {jobId: job.id, status: "FAILED", errorMessage: "Something Went Wrong"});
+
+                console.log(`[INGESTION WORKER] Job ${job.id} failed:`, err);
+            }
         }
     },
     {
@@ -24,8 +95,4 @@ worker.on("ready", () => {
 
 worker.on("completed", (job) => {
     console.log(`[INGESTION WORKER] Job ${job.id} completed`);
-});
-
-worker.on("failed", (job, err) => {
-    console.log(`[INGESTION WORKER] Job ${job.id} failed:`, err);
 });
